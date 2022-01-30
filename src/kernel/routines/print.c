8e536b6188ea93196656d82f7d410060be0c86a1
Savin Volodya <SavinVolodya001@gmail.com>
2022-01-30 17:47:19 +0200

    feat: impl print function

--- src/kernel/routines/print.c
+++ src/kernel/routines/print.c
@@ -12,20 +12,68 @@ video_char* _buff = (video_char*) 0xb8000;
 size_t _column = 0;
 size_t _row = 0;
 
+void _print_char();
+void _print_newline();
+void _clear_row();
+
 void print_clear()
 {   
-    video_char c = (video_char) {
-        c: ' '
-    };
-
     for (size_t i = 0; i < NUM_ROWS; i++)
-    {
-        for (size_t i = 0; i < NUM_COLUMNS; i++)
-            _buff[_column + NUM_COLUMNS * i] = c;
-    }
+        _clear_row(i);
 }
 
 void print_str(char *_s)
 {
+    for(char *ptr = _s; *ptr != '\0'; ptr++)
+        _print_char(*ptr);
+}
 
+void _clear_row(size_t _i)
+{
+    video_char c = (video_char) {
+        c: ' '
+    };
+
+    for (size_t j = 0; j < NUM_COLUMNS; j++)
+        _buff[_i + NUM_COLUMNS * j] = c;
+}
+
+void _print_newline()
+{
+    _column = 0;
+
+    if (row < NUM_ROWS - 1)
+    {
+        row++;
+        return;
+    }
+
+    for (size_t i = 0; i < NUM_ROWS; i++)
+    {
+        for (size_t i = 0; i < NUM_COLUMNS; i++)
+        {
+            video_char c = _buff[_column + NUM_COLUMNS * i];
+            _buff[_column + NUM_COLUMNS * (i - 1)] = c;
+        }
+    }
+
+    _clear_row(NUM_ROWS - 1);
+}
+
+void _print_char(char _c)
+{    
+    if (_c == '\n')
+    {
+        _print_newline();
+        return;
+    }
+
+    if (_column > NUM_COLUMNS)
+        _print_newline();
+
+    _buff[_column + NUM_COLUMNS * _row] = (video_char){
+        c: (uint8_t)_c,
+    }
+
+    _column++;
 }
